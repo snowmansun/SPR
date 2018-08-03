e49c2e0cd76fcdc5d37a8c23b7cfcf90dae1b380
ebest.wang <858272689@qq.com>
2018-08-03 17:33:54 +0800

    update ..

--- spr/src/main/java/com/ruoyi/project/system/person/mapper/Person.java
+++
@@ -1,5 +0,0 @@
-package com.ruoyi.project.system.person.mapper;
-
-public interface Person {
-
-}
---
+++ spr/src/main/java/com/ruoyi/project/system/person/mapper/PersonMapper.java
@@ -0,0 +1,19 @@
+package com.ruoyi.project.system.person.mapper;
+
+import com.ruoyi.project.system.person.entity.Store;
+import org.apache.ibatis.annotations.ResultType;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * 用户 UR
+ *
+ * @author ebest
+ */
+public interface PersonMapper {
+
+    @Select("select * from Store limit 10")
+    @ResultType(Store.class)
+    List<Store> getListStoreAll();
+}
